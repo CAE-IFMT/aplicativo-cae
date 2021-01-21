904c978b4a41be82cb5ada8464805adad27eae2c
Carlos Henrique <carlosheng6@gmail.com>
2021-01-21 17:14:15 -0400

    feat: adicionado controlador para teste

---
+++ lib/mock/controllers/PessoaController.dart
@@ -0,0 +1,6 @@
+import 'package:controle_de_entrada/mock/model/Pessoa.dart';
+import 'package:get/get.dart';
+
+class PessoaController extends GetxController {
+  var pessoas = List<Pessoa>().obs;
+}
