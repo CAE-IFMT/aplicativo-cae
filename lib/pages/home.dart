1f7d4eec018dab6b0b5d7392ea099c61a3032f06
Carlos Henrique <carlosheng6@gmail.com>
2021-02-02 18:27:59 -0400

    wip: adicionado botão voltar

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,6 +1,6 @@
+import 'package:controle_de_entrada/rotas/rotas.dart';
 import 'package:controle_de_entrada/widgets/lista_cadastrados.dart';
 import 'package:controle_de_entrada/widgets/qr_code_controller.dart';
-import 'package:controle_de_entrada/widgets/scan_qrcode.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -8,63 +8,73 @@ import 'package:get/get.dart';
 class Home extends GetView<QRCodeController> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false, //tira o debug da telas
-      theme: ThemeData(primarySwatch: Colors.green),
-      home: DefaultTabController(
-        length: 2,
-        child: Scaffold(
-          appBar: AppBar(
-            title: Text('Controle de Acesso IFMT'),
-            centerTitle: true,
-            textTheme:
-            Theme
-                .of(context)
-                .textTheme
-                .apply(bodyColor: Colors.black),
-            // bottom: TabBar(
-            //   tabs: <Widget>[
-            //     Tab(icon: Icon(Icons.list)),
-            //     Tab(icon: Icon(Icons.qr_code_scanner)),
-            //   ],
-            // ),
-          ),
-          // body: TabBarView(
-          //   children: containers,
-          // ),
-          body: GetBuilder(
-            init: controller,
-            builder: (controller) {
-              return controller.isOnScan ? Center(
-                child: Text(
-                  controller.result,
-                  style: new TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-                ),
-              ) :
-              Container(
-                child: ListaCadastrados(),
-              );
-            },
-          ),
-          floatingActionButton: FloatingActionButton.extended(
-            icon: Icon(Icons.qr_code_scanner),
-            label: Text('Scan'),
-            onPressed: () => controller.scanQR(),
+    return WillPopScope(
+      onWillPop: _onBackPressed,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        //tira o debug da telas
+        theme: ThemeData(primarySwatch: Colors.green),
+        home: DefaultTabController(
+          length: 2,
+          child: Scaffold(
+            appBar: AppBar(
+              title: Text('Controle de Acesso IFMT'),
+              centerTitle: true,
+              textTheme:
+                  Theme.of(context).textTheme.apply(bodyColor: Colors.black),
+            ),
+            body: GetBuilder(
+              init: controller,
+              builder: (controller) {
+                return controller.isOnScan ? _telaScan(context) : _telaLista();
+              },
+            ),
+            floatingActionButton: FloatingActionButton.extended(
+              icon: Icon(Icons.qr_code_scanner),
+              label: Text('Scan'),
+              onPressed: () => controller.scanQR(),
+            ),
           ),
         ),
       ),
     );
   }
 
+  _telaScan(BuildContext context) {
+    return Center(
+      child: Text(
+        controller.result,
+        style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+      ),
+    );
+  }
 
+  _telaLista() {
+    return Container(
+      child: ListaCadastrados(),
+    );
+  }
 
-
-// List<Widget> containers = [
-//   Container(
-//     child: ListaCadastrados(),
-//   ),
-//   Container(
-//    child: ScanQRCode(),
-//   ),
-// ];
+  Future<bool> _onBackPressed() {
+    print('entrou nmo bagui');
+    return showDialog(
+      context: Get.context,
+      builder: (context) => new AlertDialog(
+        title: Text('Deseja Sair?'),
+        content: Text('Você será direcionado para a tela de login!'),
+        actions: [
+          FlatButton(
+            onPressed: () => Navigator.of(context).pop(false),
+            child: Text('Não'),
+            color: Colors.green,
+          ),
+          FlatButton(
+            onPressed: () => Get.toNamed(Routes.LOGIN),
+            child: Text('Sim'),
+            color: Colors.green,
+          ),
+        ],
+      ),
+    );
+  }
 }
