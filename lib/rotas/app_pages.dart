53882126ac12261e38e579af3e64c46f9e1ee74c
Carlos Henrique <carlosheng6@gmail.com>
2021-02-02 18:29:11 -0400

    feat: implementado sistema de rotas

---
+++ lib/rotas/app_pages.dart
@@ -0,0 +1,13 @@
+import 'package:controle_de_entrada/pages/home.dart';
+import 'package:controle_de_entrada/pages/login.dart';
+import 'package:controle_de_entrada/rotas/rotas.dart';
+import 'package:get/route_manager.dart';
+
+class AppPages{
+  static final routes = [
+    GetPage(name: Routes.LOGIN,
+      page: () => Login()),
+    GetPage(name: Routes.HOME,
+        page: () => Home()),
+  ];
+}
