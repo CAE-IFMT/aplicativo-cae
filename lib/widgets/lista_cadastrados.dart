f74a661ab5fb125bcc3018bda8dc0c3e2f1fa69f
Carlos Henrique <carlosheng6@gmail.com>
2021-02-09 20:01:35 -0400

    refactor: altera o controler para visitas

--- lib/widgets/lista_cadastrados.dart
+++ lib/widgets/lista_cadastrados.dart
@@ -1,47 +1,48 @@
-import 'package:controle_de_entrada/conect_API/controllers/PessoaController.dart';
+import 'package:controle_de_entrada/conect_API/controllers/visita_controller.dart';
 import 'package:controle_de_entrada/pages/informacoes.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class ListaCadastrados extends GetView<PessoaController> {
+class ListaCadastrados extends GetView<VisitaController> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: _buildListView(),
+    return Container(
+      child: _buildListView(),
     );
   }
 
   _buildListView() {
     return GetBuilder(
-        init: controller,
-        initState: (state) => controller.fetch(),
-        builder: (controller) {
-          return ListView.separated(
-            itemCount: controller.pessoas.length,
-            separatorBuilder: (BuildContext context, int index) => Divider(
-              height: 3.0,
-            ),
-            itemBuilder: (context, index) {
-              return ListTile(
-                title: Text(controller.pessoas[index].name),
-                subtitle: Text('CPF: ${controller.pessoas[index].cpf}'),
-                leading: CircleAvatar(
-                  child: Icon(Icons.person),
-                ),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => DadosPessoa(
-                        controller.pessoas[index],
-                      ),
+      init: controller,
+      initState: (state) => controller.fetch(),
+      builder: (controller) {
+        return ListView.separated(
+          itemCount: controller.visitas.length,
+          separatorBuilder: (BuildContext context, int index) => Divider(
+            height: 3.0,
+          ),
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(controller.visitas[index].visitante.nome),
+              subtitle: Text('CPF: ${controller.visitas[index].visitante.cpf}'),
+              leading: CircleAvatar(
+                child: Text(
+                    controller.visitas[index].visitante.nome.substring(0, 1)),
+              ),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => DadosPessoa(
+                      visitaModel: controller.visitas[index],
                     ),
-                  );
-                },
-              );
-            },
-          );
-        });
+                  ),
+                );
+              },
+            );
+          },
+        );
+      },
+    );
   }
 }
