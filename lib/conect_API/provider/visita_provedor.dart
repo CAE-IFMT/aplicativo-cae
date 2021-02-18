26855632954e9faa0464a774dd39758460e260c7
Carlos Henrique <carlosheng6@gmail.com>
2021-02-18 17:03:59 -0400

    feat: adicionado busca por id

--- lib/conect_API/provider/visita_provedor.dart
+++ lib/conect_API/provider/visita_provedor.dart
@@ -4,7 +4,7 @@ import 'package:flutter/cupertino.dart';
 
 import 'abstract_visita_model.dart';
 
-const api = 'http://192.168.0.106:5000';
+const api = 'https://cae-ifmt.herokuapp.com';  //'http://192.168.0.106:5000';
 
 class VisitaProvider implements AbstractVisitaProvider{
   final Dio dio;
@@ -15,8 +15,18 @@ class VisitaProvider implements AbstractVisitaProvider{
   Future<List<VisitaModel>> fetchVisita() async {
     final response = await dio.get('$api/visitas');
     final visitaModel = response.data.map<VisitaModel>((data) => VisitaModel.fromJson(data)).toList();
-    print(visitaModel);
-    print(response.data);
+    if (response.statusCode == 200) {
+      return visitaModel;
+    } else {
+      print('Erro método GET');
+      throw Exception('Erro  método GET');
+    }
+  }
+
+  @override
+  Future<VisitaModel> fetchById(int id) async{
+    final response = await dio.get('$api/visitas/$id');
+    final visitaModel = VisitaModel.fromJson(response.data);
     if (response.statusCode == 200) {
       return visitaModel;
     } else {
