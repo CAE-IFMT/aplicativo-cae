8c09afec6aab02104e9173a0615e67c44ca76dae
Carlos Henrique <carlosheng6@gmail.com>
2021-02-18 17:09:52 -0400

    wip: em desenvolvimento

--- lib/conect_API/login_api.dart
+++ lib/conect_API/login_api.dart
@@ -5,30 +5,30 @@ import 'package:http/http.dart' as http;
 class LoginApi{
   static Future<bool> login(String user, String password) async {
     //var url = 'https://carros-springboot.herokuapp.com/api/v2/login'; //alterar url
-    var url = 'http://192.168.0.106:8080/usuarios';
+    //var url = 'http://192.168.0.106:8080/visita';
 
-    var header = {"Content-Type" : "application/json"};
+    // var header = {"Content-Type" : "application/json"};
+    //
+    // Map params = {
+    //   "login": user,
+    //   "senha": password
+    // };
+    // var _body = json.encode(params);
+    //
+    // print('json $_body');
+    //
+    // var response = await http.get(url, headers: header);
+    //
+    //
+    // print('Status ${response.statusCode}');
+    // print('Bory  ${response.body}');
 
-    Map params = {
-      "login": user,
-      "senha": password
-    };
-    var _body = json.encode(params);
-
-    print('json $_body');
-
-    var response = await http.get(url, headers: header);
-    
-
-    print('Status ${response.statusCode}');
-    print('Bory  ${response.body}');
-
-    if(response.statusCode == 200){
+    //if(response.statusCode == 200){
       return true;
-     }
-     else{
-       return false;
-     }
+     //}
+    //else{
+      // return false;
+     //}
 
   }
 }
