a82e27a0a86d3e4e817d6f66977850fa6e842be0
Carlos Henrique <carlosheng6@gmail.com>
2021-02-18 17:02:12 -0400

    feat: adicionado retorno de VisitaModel

--- lib/conect_API/controllers/visita_controller.dart
+++ lib/conect_API/controllers/visita_controller.dart
@@ -1,19 +1,55 @@
+import 'package:barcode_scan/barcode_scan.dart';
 import 'package:controle_de_entrada/conect_API/model/visita.dart';
 import 'package:controle_de_entrada/conect_API/repository/visita_repository.dart';
+import 'package:controle_de_entrada/rotas/rotas.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 
 class VisitaController extends GetxController {
-  var _visitas = List<VisitaModel>();
   VisitaRepository repository;
-
   VisitaController({@required this.repository});
 
-  Future<VisitaModel> fetch() async{
+  List<VisitaModel> _visitas;
+  List<VisitaModel> get visitas => this._visitas;
+
+  bool _isOnScan = false;
+  bool get isOnScan => _isOnScan;
+  set isOnScan(bool value) {
+    _isOnScan = value;
+    update();
+  }
+
+
+  String _result;
+  String get result => _result;
+
+  Future<VisitaModel> fetch() async {
     final visitas = await this.repository.fetchVisita();
     this._visitas = visitas;
     update();
   }
 
-  List<VisitaModel> get visitas => this._visitas;
+  Future scanQR() async {
+    try {
+      _result = (await BarcodeScanner.scan());
+      _isOnScan = true;
+    } on PlatformException catch (ex) {
+      if (ex.code == BarcodeScanner.CameraAccessDenied) {
+        _result = 'A permissão da câmera foi negada!';
+        _isOnScan = false;
+      } else {
+        _result = 'Erro desconhecido $ex';
+        _isOnScan = false;
+      }
+    } on FormatException {
+      Get.toNamed(Routes.HOME);
+      _isOnScan = false;
+    } catch (ex) {
+      _result = 'Erro desconhecido $ex';
+      _isOnScan = false;
+    } finally{
+      update();
+    }
+  }
 }
