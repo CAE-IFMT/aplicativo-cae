d1af300739a7c962b14db1a8f1cabb39ca76dae0
Carlos Henrique <carlosheng6@gmail.com>
2021-02-25 14:34:38 -0400

    reafactor: altera de card para container

--- lib/modules/widgets/body_qr_code.dart
+++ lib/modules/widgets/body_qr_code.dart
@@ -29,58 +29,63 @@ class QRCodeResult extends GetView<ScanController> {
     );
   }
 
-  Card _detalhes(ScanController controller) {
-    return Card(
-      child: Column(
-        children: [
-          _listTile('Nome:', controller.visita.visitante.nome),
-          _listTile('Professor Responsável:', controller.visita.professor.nome),
-          _listTile('CPF:', controller.visita.visitante.cpf),
-          _listTile('E-mail:', controller.visita.visitante.email),
-          _listTile('Justificativa para acesso:', controller.visita.motivo),
-          _listTile('Data requerida:', controller.visita.data),
-          _listTile('Ocorrido:', controller.visita.ocorrido.toString()),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
+  _detalhes(ScanController controller) {
+    return Column(
+      children: [
+        Container(
+          margin: EdgeInsets.only(bottom: 50.0, left: 20.0,top: 10.0),
+          child: Column(
             children: [
-              ButtonTheme(
-                minWidth: 100.0,
-                height: 50.0,
-                child: RaisedButton(
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20.0),
-                      side: BorderSide(color: Colors.red)),
-                  child: Text(
-                    'Voltar',
-                    style: TextStyle(color: Colors.white, fontSize: 17.0),
-                  ),
-                  color: Colors.red,
-                  onPressed: () {
-                    _returnHome();
-                  },
-                ),
-              ),
-              ButtonTheme(
-                minWidth: 100.0,
-                height: 50.0,
-                child: RaisedButton(
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20.0),
-                      side: BorderSide(color: Colors.green)),
-                  child: _flagAutorizar(controller.flagAutorizar),
-                  color: Colors.green,
-                  onPressed: () async {
-                    controller.flagAutorizar = true;
-                    await controller
-                        .atualizaStatusOcorrido(controller.visita.id);
-                    await _returnHome();
-                  },
-                ),
-              ),
+              _listTile('Nome:', controller.visita.visitante.nome),
+              _listTile(
+                  'Professor Responsável:', controller.visita.professor.nome),
+              _listTile('CPF:', controller.visita.visitante.cpf),
+              _listTile('E-mail:', controller.visita.visitante.email),
+              _listTile('Justificativa para acesso:', controller.visita.motivo),
+              _listTile('Data requerida:', controller.visita.data),
             ],
           ),
-        ],
-      ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            ButtonTheme(
+              minWidth: 100.0,
+              height: 50.0,
+              child: RaisedButton(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                    side: BorderSide(color: Colors.red)),
+                child: Text(
+                  'Voltar',
+                  style: TextStyle(color: Colors.white, fontSize: 17.0),
+                ),
+                color: Colors.red,
+                onPressed: () {
+                  _returnHome();
+                },
+              ),
+            ),
+            ButtonTheme(
+              minWidth: 100.0,
+              height: 50.0,
+              child: RaisedButton(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                    side: BorderSide(color: Colors.green)),
+                child: _flagAutorizar(controller.flagAutorizar),
+                color: Colors.green,
+                onPressed: () async {
+                  controller.flagAutorizar = true;
+                  await controller
+                      .atualizaStatusOcorrido(controller.visita.id);
+                  await _returnHome();
+                },
+              ),
+            ),
+          ],
+        ),
+      ],
     );
   }
 
@@ -101,7 +106,8 @@ class QRCodeResult extends GetView<ScanController> {
   Future<void> _returnHome() async {
     VisitaController visitaController = Get.find();
     visitaController.isOnScan = false;
-    await visitaController.fetch();
+    await visitaController.fetch('ocorrido');
+    await visitaController.fetch('naoOcorrido');
     controller.flagAutorizar = false;
     Get.toNamed(Routes.HOME);
   }
@@ -111,11 +117,6 @@ class QRCodeResult extends GetView<ScanController> {
   }
 
   _listTile(String title, String subTitle) {
-    if (subTitle == 'true') {
-      subTitle = 'Sim';
-    } else if (subTitle == 'false') {
-      subTitle = 'Não';
-    }
     return ListTile(
       title: Text(title),
       subtitle: Text(subTitle),
